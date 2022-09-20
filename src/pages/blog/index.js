6f8a7dc90f29af77e3906769b7154d04a5e42cf4
mattmuroya <matt.muroya@gmail.com>
2022-09-19 16:32:28 -1000

    tutorial part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import Layout from '../components/layout';
-import Seo from '../components/seo';
-import { graphql } from 'gatsby';
+import Layout from '../../components/layout';
+import Seo from '../../components/seo';
+import { Link, graphql } from 'gatsby';
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <Link to={`./${node.frontmatter.slug}`}>
+            <h2>{node.frontmatter.title}</h2>
+          </Link>
           <p>Posted: {node.frontmatter.date}</p>
           <p>Updated: {node.parent.modifiedTime}</p>
           <p>{node.excerpt}</p>
@@ -25,6 +27,7 @@ export const query = graphql`
         frontmatter {
           title
           date(formatString: "MMM DD, YYYY")
+          slug
         }
         id
         excerpt
