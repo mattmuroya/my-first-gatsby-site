18c79bf1425dba0de4d4d79e5234330dc394187a
mattmuroya <matt.muroya@gmail.com>
2022-09-20 10:39:29 -1000

    tutorial part 7

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
